57426267467e921ae9ec6b041440497203bdc80a
James Trask <pair+james@pivotallabs.com>
2013-02-18 12:56:10 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whenever::Application.config.secret_token = '577490a50583f25065579be2bb35faa6a9183c0d5fab6ab36d5d6cb0cfde08c93a01a74c2686d0cc531d21e144e6dfac2f96528238aa4adb87f9d6dfab47a88a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Whenever::Application.config.session_store :cookie_store, key: '_whenever_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Whenever::Application.config.session_store :active_record_store
