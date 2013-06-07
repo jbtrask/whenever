beff543323f4924a63ff070d04410dbcc2a7960b
James Trask <jbt_5@yahoo.com>
2013-06-06 20:14:46 -0700

    added presence detection

--- script/ping.rb
+++ script/ping.rb
@@ -5,6 +5,12 @@ i = 0
 loop do
 	i += 1
 	ping_result = `sudo l2ping -c 1 #{JBT_IPHONE}`
-	puts "#{i}:  #{DateTime.now}\n\n#{ping_result}\n\n"
+	puts "#{i}:  #{DateTime.now.to_s}\n\n#{ping_result}\n\n"
+  if ping_result.match(/1 sent, 1 received/)
+    puts "PRESENT\n\n"
+    File.open('tmp/last.log', 'w+') do |f|
+      f.write(DateTime.now.to_s)
+    end
+  end
 	sleep PERIOD
 end
