46de6c12045cdcce480f7e24bba66b3bc7370ef5
James Trask <jbt_5@yahoo.com>
2013-06-06 21:17:34 -0700

    changed from DateTime to Time object(s) (better ruby support)

--- script/ping.rb
+++ script/ping.rb
@@ -1,17 +1,18 @@
 JBT_IPHONE = 'E4:25:E7:C0:52:BB'
 PERIOD = 10 #seconds
+AWAY_TIME = 60 #seconds
 
 i = 0
 loop do
 	i += 1
 	ping_result = `sudo l2ping -c 1 #{JBT_IPHONE}`
-	puts "#{i}:  #{DateTime.now.to_s}\n\n#{ping_result}\n\n"
+	puts "#{i}:  #{Time.now.to_s}\n\n#{ping_result}\n\n"
   if ping_result.match(/1 sent, 1 received/)
     puts "PRESENT\n\n"
     File.open('tmp/last.log', 'w+') do |f|
-      f.write(DateTime.now.to_s)
+      f.write(Time.now.to_s)
     end
-  elsif(DateTime.now - DateTime.parse(File.read('tmp/last.log')) > 1.minute)
+  elsif(Time.now - Time.parse(File.read('tmp/last.log')) > AWAY_TIME)
     puts "LONG ABSENCE\n\n"
   end
 	sleep PERIOD
