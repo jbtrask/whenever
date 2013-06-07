f470acba80fcaa0545e9d7dd3bc77e0241fb32da
James Trask <jbt_5@yahoo.com>
2013-06-06 19:55:27 -0700

    adjusted period; added timestamp

--- script/ping.rb
+++ script/ping.rb
@@ -1,9 +1,10 @@
 JBT_IPHONE = 'E4:25:E7:C0:52:BB'
+PERIOD = 10 #seconds
 
 i = 0
 loop do
 	i += 1
 	ping_result = `sudo l2ping -c 1 #{JBT_IPHONE}`
-	puts "#{i}\n\n#{ping_result}\n\n"
-	sleep 5
+	puts "#{i}:  #{DateTime.now}\n\n#{ping_result}\n\n"
+	sleep PERIOD
 end
