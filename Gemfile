bf762cf9ebd1cc0e42bc261a3c8dc95036fa9c9f
James Trask <jbt_5@yahoo.com>
2013-06-09 22:28:03 -0700

    moved email settings to env

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails'
 gem 'jquery-rails'
 gem 'psych'
+gem 'dotenv'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/environment.rb
+++ config/environment.rb
@@ -9,11 +9,11 @@ Whenever::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-      :address  => 'smtp.cox.net',
-      :port  => 587,
-      :domain => 'trask.net',
-      :user_name  => 'jbtrask@cox.net',
-      :password  => 'jbt123Zjbt123Z',
+      :address  => ENV['EMAIL_SERVER'],
+      :port  => ENV['EMAIL_PORT'],
+      :domain => ENV['EMAIL_DOMAIN'],
+      :user_name  => ENV['EMAIL_USERNAME'],
+      :password  => ENV['EMAIL_PASSWORD'],
       :authentication  => :login,
       :enable_starttls_auto => true
   }
