2246a455f5f37212769bc0713f6887efea38f2bc
James Trask <jbt_5@yahoo.com>
2013-06-09 19:11:54 -0700

    fixed sunset/rise bug; refactored messaging

--- script/ping.rb
+++ script/ping.rb
@@ -5,13 +5,10 @@ SUNRISE_TIME = '5:40:AM' # 6/7/13
 SUNSET_TIME = '7:54PM' # 6/7/13
 DATE_FORMAT = '%Y-%m-%d %l:%M:%S%p (%a)'
 
-today = DateTime.now.in_time_zone.to_date
-@sunrise_time = Time.zone.parse("#{today} #{SUNRISE_TIME}")
-@sunset_time = Time.zone.parse("#{today} #{SUNSET_TIME}")
 @on_time = @off_time = @last_seen_time = nil
 
 def is_dark?
-  Time.now < @sunrise_time || Time.now > @sunset_time
+  Time.now < sunrise_time || Time.now > sunset_time
 end
 
 def lights_off
@@ -32,40 +29,47 @@ i = 0
 loop do
 	i += 1
   ping_result = `sudo l2ping -c 1 #{JBT_IPHONE}`
-  puts "#{i}:  #{now}\n\n#{ping_result}\n\n"
+  today = DateTime.now.in_time_zone.to_date
+  sunrise_time = Time.zone.parse("#{today} #{SUNRISE_TIME}")
+  sunset_time = Time.zone.parse("#{today} #{SUNSET_TIME}")
+  action = nil
   if ping_result.match(/1 sent, 1 received/)
-    puts "PRESENT\n\n"
+    status = "PRESENT"
     @last_seen_time = Time.now
     if is_dark?
       if @on_time.blank? || (@off_time.present? && @off_time > @on_time)
-        puts "ON\n\n"
+        action = "ON"
         @on_time = @last_seen_time
         lights_on
       end
     end
   elsif @last_seen_time.blank? || Time.now - @last_seen_time > AWAY_TIME * 60 #sec/min
+    status = "MISSING"
     if @off_time.blank? || (@on_time.present? && @off_time < @on_time)
-      puts "OFF\n\n"
+      action = "OFF"
       @off_time = Time.now
       lights_off
     end
   else
-    puts "RECENTLY MISSING\n\n"
+    status = "RECENTLY MISSING"
   end
 
-  puts ping_result.match(/1 sent, 1 received/)
   unless ping_result.match(/1 sent, 1 received/)
-    puts " not seen:  #{((Time.now - @last_seen_time) / 60.0).round(2)}" if @last_seen_time.present?
+    status += " (#{((Time.now - @last_seen_time) / 60.0).round(2)} minutes)" if @last_seen_time.present?
   end
 
-  puts " now:  #{now}"
-  puts " sunrise:  #{@sunrise_time.try{|t| t.strftime(DATE_FORMAT)}}"
-  puts " sunset:  #{@sunset_time.try{|t| t.strftime(DATE_FORMAT)}}"
-  puts " is dark:  #{is_dark?}"
-  puts " on:  #{@on_time.try{|t| t.strftime(DATE_FORMAT)}}"
-  puts " off:  #{@off_time.try{|t| t.strftime(DATE_FORMAT)}}"
-  puts " last seen:  #{@last_seen_time.try{|t| t.strftime(DATE_FORMAT)}}"
+  puts "#{i}:  #{now}"
+  puts "   status:  #{status}"
+  puts "   last seen:  #{@last_seen_time.try{|t| t.strftime(DATE_FORMAT)}}"
+  puts "   sunrise:  #{sunrise_time.try{|t| t.strftime(DATE_FORMAT)}}"
+  puts "   sunset:  #{sunset_time.try{|t| t.strftime(DATE_FORMAT)}}"
+  puts "   is dark:  #{is_dark?}"
+  puts "   on:  #{@on_time.try{|t| t.strftime(DATE_FORMAT)}}"
+  puts "   off:  #{@off_time.try{|t| t.strftime(DATE_FORMAT)}}"
+  puts "   action:  #{action}" if action.present?
   puts "\n\n"
-	sleep PERIOD
+
+  sleep PERIOD
+
 end
 
