2adde57fd116a2aa3babe579a4aa3666affd2122
James Trask <jbt_5@yahoo.com>
2013-06-09 21:35:42 -0700

    refactored results into csv

--- script/ping.rb
+++ script/ping.rb
@@ -1,3 +1,5 @@
+include ActionView::Helpers::DateHelper
+
 JBT_IPHONE = 'E4:25:E7:C0:52:BB'
 PERIOD = 15 # seconds
 AWAY_TIME = 2 # minutes
@@ -28,44 +30,64 @@ def now
   DateTime.now.in_time_zone.strftime(DATE_FORMAT)
 end
 
+def time_ago(reference_time)
+  if reference_time.present?
+    "#{reference_time.strftime(DATE_FORMAT)} (#{time_ago_in_words(reference_time, include_seconds: true)})"
+  else
+    'NEVER'
+  end
+end
+
+header = %w{
+  idx
+  time
+  status
+  action
+  dark
+  last
+  on
+  off
+}
+puts header.join(', ')
+
 i = 0
 loop do
 	i += 1
   ping_result = `sudo l2ping -c 1 #{JBT_IPHONE}`
-  action = nil
+  action = 'NONE'
   if ping_result.match(/1 sent, 1 received/)
-    status = "PRESENT"
+    status = 'PRESENT'
     @last_seen_time = Time.now
     if is_dark?
       if @on_time.blank? || (@off_time.present? && @off_time > @on_time)
-        action = "ON"
+        action = 'ON'
         @on_time = @last_seen_time
         lights_on
       end
     end
-  elsif @last_seen_time.blank? || Time.now - @last_seen_time > AWAY_TIME * 60 #sec/min
-    status = "MISSING"
-    if @off_time.blank? || (@on_time.present? && @off_time < @on_time)
-      action = "OFF"
-      @off_time = Time.now
-      lights_off
-    end
   else
-    status = "RECENTLY MISSING"
+    status = 'MISSING'
+    if @last_seen_time.blank? || Time.now - @last_seen_time > AWAY_TIME * 60 #sec/min
+      if @off_time.blank? || (@on_time.present? && @off_time < @on_time)
+        action = "OFF"
+        @off_time = Time.now
+        lights_off
+      end
+    end
   end
 
-  unless ping_result.match(/1 sent, 1 received/)
-    status += " (#{((Time.now - @last_seen_time) / 60.0).round(2)} minutes)" if @last_seen_time.present?
-  end
-
-  puts "#{i}:  #{now}"
-  puts "   status:  #{status}"
-  puts "   last seen:  #{@last_seen_time.try{|t| t.strftime(DATE_FORMAT)}}"
-  puts "   is dark:  #{is_dark?}"
-  puts "   on:  #{@on_time.try{|t| t.strftime(DATE_FORMAT)}}"
-  puts "   off:  #{@off_time.try{|t| t.strftime(DATE_FORMAT)}}"
-  puts "   action:  #{action}" if action.present?
-  puts "\n\n"
+  report = 
+  [
+    i,
+    now,
+    status,
+    action,
+    is_dark?,
+    time_ago(@last_seen_time),
+    time_ago(@on_time),
+    time_ago(@off_time)
+  ]
+  puts report.join(', ')
 
   sleep PERIOD
 
