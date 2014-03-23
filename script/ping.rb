4525ea026ada56a47939328f1562f44d44d440a7
James Trask <jbt_5@yahoo.com>
2014-03-22 17:31:50 -0700

    more tweaks to ping.rb script

--- script/ping.rb
+++ script/ping.rb
@@ -34,49 +34,54 @@ puts header.join(', ')
 i = 0
 loop do
 
-  i += 1
-  @now = DateTime.now.in_time_zone
+  begin
+    i += 1
+    @now = DateTime.now.in_time_zone
 
-  ping_result = `sudo l2ping -c 1 #{ENV['BLUETOOTH_MAC']}`
+    ping_result = %x{ sudo l2ping -c 1 #{ENV['BLUETOOTH_MAC']} }
 
-  action = 'none'
-  if ping_result.match(/1 sent, 1 received/)
-    status = 'present'
-    @last_seen = @now
-    if is_dark?
-      if @on.blank? || (@off.present? && @off > @on)
-        @on = @now
-        action = 'on'
-        Light.all_on
+    action = 'none'
+    if ping_result.match(/1 sent, 1 received/)
+      status = 'present'
+      @last_seen = @now
+      if is_dark?
+        if @on.blank? || (@off.present? && @off > @on)
+          @on = @now
+          action = 'on'
+          Light.all_on
+        end
+      end
+    else
+      status = 'missing'
+      if @last_seen.blank? || @now - @last_seen > AWAY * 60 #sec/min
+        if @off.blank? || (@on.present? && @off < @on)
+          @off = @now
+          action = 'off'
+          Light.all_off
+        end
       end
     end
-  else
-    status = 'missing'
-    if @last_seen.blank? || @now - @last_seen > AWAY * 60 #sec/min
-      if @off.blank? || (@on.present? && @off < @on)
-        @off = @now
-        action = 'off'
-        Light.all_off
-      end
-    end
+
+    report =
+      [
+        i,
+        @now.strftime(DATE_FORMAT[:display]),
+        @now.strftime(DATE_FORMAT[:db]),
+        status,
+        is_dark? ? 'dark' : 'light',
+        action,
+        @last_seen.try{|t| time_ago_in_words(t, include_seconds: true)} || '--- ',
+        @last_seen.try{|t| t.strftime(DATE_FORMAT[:db])} || 'never seen',
+        @on.try{|t| t.strftime(DATE_FORMAT[:db])} || 'never on',
+        @off.try{|t| t.strftime(DATE_FORMAT[:db])} || 'never off'
+      ]
+    puts report.join(', ')
+
+  rescue Exception => ex
+    puts "error:  #{ex.message}"
+  ensure
+    sleep ENV['BLUETOOTH_PERIOD'].to_f
   end
-
-  report =
-  [
-    i,
-    @now.strftime(DATE_FORMAT[:display]),
-    @now.strftime(DATE_FORMAT[:db]),
-    status,
-    is_dark? ? 'dark' : 'light',
-    action,
-    @last_seen.try{|t| time_ago_in_words(t, include_seconds: true)} || '--- ',
-    @last_seen.try{|t| t.strftime(DATE_FORMAT[:db])} || 'never seen',
-    @on.try{|t| t.strftime(DATE_FORMAT[:db])} || 'never on',
-    @off.try{|t| t.strftime(DATE_FORMAT[:db])} || 'never off'
-  ]
-  puts report.join(', ')
-
-  sleep ENV['BLUETOOTH_PERIOD'].to_f
 
 end
 
